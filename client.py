d4c3882e56e4943db036d3c50f3bc2b8c4edd4b3
Kenneth Hunter <kennethdhunter@gmail.com>
2020-10-24 14:50:47 -0600

    Updated call config for python client.

--- client.py
+++ client.py
@@ -1,19 +1,28 @@
 import socket
 
 client_socket = None
-address = "127.0.0.1"
+address = "192.168.72.1" # This will be the server's address on the local network. It is imperative that the client is already on the hostapd network! 
 default_port = 12873
 id = -1
 ACK = 0x0E
 game_in_progress = False
 message_size = 64
+READY = 1
+BUSY = 2
+DEV_ADDED = 3
+DEV_REMOVED = 4
+
 
 def setupClient():
     global client_socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Attempting to connect to server...")
-    client_socket.connect(("192.168.1.130", default_port))
+    client_socket.connect((address, default_port))
     id = int.from_bytes(client_socket.recv(message_size), byteorder='big')
-    print("Got ID %d" % id)
+    status = int.from_bytes(client_socket.recv(message_size), byteorder='big')
+    if status == ACK:
+        print("Got ID %d" % id)
 
-setupClient()
+
+if __name__ == "__main__":
+    setupClient()
